b5d299d91a3a92c4d1ea94eba613e5b22bc2686f
jsl9208 <shilong@heywhale.com>
2024-08-17 10:28:19 +0800

    update

---
+++ .config/hypr/custom/switch.sh
@@ -0,0 +1,9 @@
+#!/usr/bin/env bash
+
+if grep open /proc/acpi/button/lid/LID0/state; then
+	hyprctl keyword monitor "eDP-2,prefered,auto,2"
+else
+	if [[ $(hyprctl monitors | grep "Monitor" | wc -l) != 1 ]]; then
+		hyprctl keyword monitor "eDP-2,disable"
+	fi
+fi
