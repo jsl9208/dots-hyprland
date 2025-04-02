c7d8ced60355839466a3b0de1c22a0708aeaffe3
jsl9208 <shilong@heywhale.com>
2025-04-02 14:22:40 +0800

    migration window rules

--- .config/hypr/custom/switch.sh
+++ .config/hypr/custom/switch.sh
@@ -1,9 +1,11 @@
 #!/usr/bin/env bash
 
 if grep open /proc/acpi/button/lid/LID0/state; then
-	hyprctl keyword monitor "eDP-2,prefered,auto,2"
+  hyprctl keyword monitor "eDP-1,prefered,auto,2"
+  hyprctl keyword monitor "eDP-2,prefered,auto,2"
 else
-	if [[ $(hyprctl monitors | grep "Monitor" | wc -l) != 1 ]]; then
-		hyprctl keyword monitor "eDP-2,disable"
-	fi
+  if [[ $(hyprctl monitors | grep "Monitor" | wc -l) != 1 ]]; then
+    hyprctl keyword monitor "eDP-1,disable"
+    hyprctl keyword monitor "eDP-2,disable"
+  fi
 fi
